5c733bb57aa66f87e3a5f8d33b21ec875922ae66
Tony Baker <this.tonybaker@gmail.com>
2019-02-01 22:19:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last_element = array.pop  #take the last element off and save
+    with_commas = array.join(", ") #array turns into a string with commas
+    with_commas + ", and " + last_element  #concat string and last element
+  else
+    array.join
+  end
 end
