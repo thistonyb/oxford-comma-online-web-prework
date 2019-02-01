064c6bc3e906053c64dc32e0e456ffefd397dcfd
Tony Baker <this.tonybaker@gmail.com>
2019-02-01 22:03:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length > 2
     last = array.pop
     with_commas = array.join(",")
-    with_commas.push("and" + last)
+    with_commas.push("and" last)
   else
     array.join
 
